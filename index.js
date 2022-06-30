a98f24506b569cda27ed3d8f857bc3c1cd62ea40
Gabriel Burgos <gabrielburgosmm@gmail.com>
2022-06-30 13:41:11 -0700

    commit tree d08c187a042e54dff039a20b69b5c4c32602936d

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+
+const totalBatteries = batteryBatches.reduce(function(total,batches) {
+    return batches + total
+})
+
+function goat() {
+    const goat = "is"
+    return goat
+}
+
+
+function gh() {
+    const goat = "jj"
+    return goat
+}
